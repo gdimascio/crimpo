cf42a936e6120bf955e74f711b0f9fe0756d6a12
gdimascio <guido.dimascio@gmail.com>
2023-06-11 15:43:40 -0300

    credenciales fb

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
 
 const admin = require ("firebase-admin");
 
-const serviceAccount = require("../crimpo-79cbe-firebase-adminsdk-pp8q2-a9e3f3137e.json");
+const serviceAccount = require("../crimpo-79cbe-firebase-adminsdk-pp8q2-a71f59cd66.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });

0137b35f9937f6ad193e6cb4e6f51ae7eebf068d
gdimascio <guido.dimascio@gmail.com>
2023-06-24 12:10:56 -0300

    x

--- controllers/controllerAdmin.js
+++ controllers/controllerAdmin.js
@@ -7,67 +7,62 @@ const catalogoCollection = db.collection("catalogo");
 function getCollection(producto){return db.collection(producto)}
 
 
-const controller = {
-        //LIST ADMIN
-        listAdmin: async(req, res) => {
-            const catalogoSnapshot = await catalogoCollection.get();
-            const catalogo = catalogoSnapshot.docs.map(doc => ({
-                id: doc.id, 
-                ...doc.data()
-            }))
-            res.render("admin", {catalogo})
-        },
+//LIST ADMIN
+exports.listAdmin = async(req, res) => {
+    const catalogoSnapshot = await catalogoCollection.get();
+    const catalogo = catalogoSnapshot.docs.map(doc => ({
+        id: doc.id, 
+        ...doc.data()
+    }))
+    res.render("admin", {catalogo})
+},
 
-        // SELECT CATEGORIA
-        cargar: async(req, res) => {
-            const categoria = req.body.categorias;
-            const productoCollection = getCollection(categoria);
-            const productoSnapshot = await productoCollection.get();
-            const producto = productoSnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-            res.render("admin_cargar", {categoria})  
-            // esto redireccionó a admin_cargar pasandole toda la lista de *producto*
-        },
-        cargarProducto: async(req, res) => {
-            const {modelo, tamano, precio} = req.body;
-            const categoria = req.body.cat;
-            const newProducto = {modelo, tamano, precio};
-            const productoCollection = getCollection(categoria);
-            await productoCollection.add(newProducto);
-            res.redirect("/admin");
-        },
+// SELECT CATEGORIA
+exports.cargar = async(req, res) => {
+    const categoria = req.body.categorias;
+    const productoCollection = getCollection(categoria);
+    const productoSnapshot = await productoCollection.get();
+    const producto = productoSnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+    res.render("admin_cargar", {categoria})  
+    // esto redireccionó a admin_cargar pasandole toda la lista de *producto*
+},
+exports.cargarProducto = async(req, res) => {
+    const {modelo, tamano, precio} = req.body;
+    const categoria = req.body.cat;
+    const newProducto = {modelo, tamano, precio};
+    const productoCollection = getCollection(categoria);
+    await productoCollection.add(newProducto);
+    res.redirect("/admin");
+},
 
-        editar: async(req, res) => {
-            const productoNombre = req.body.categorias;
-            const productoCollection = getCollection(productoNombre);
-            const productoSnapshot = await productoCollection.get();
-            const producto = productoSnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-            res.render("admin_editar", {producto, productoNombre})  
-        },
+exports.editar = async(req, res) => {
+    const productoNombre = req.body.categorias;
+    const productoCollection = getCollection(productoNombre);
+    const productoSnapshot = await productoCollection.get();
+    const producto = productoSnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+    res.render("admin_editar", {producto, productoNombre})  
+},
 
-        borrar:async(req, res) => {
-            const dlteId = req.body.id;
-            const categoria = req.body.cat;
-            const productoCollection = getCollection(categoria);
-            await productoCollection.doc(dlteId).delete();
-            res.redirect("/admin");
-        },
+exports.borrar = async(req, res) => {
+    const dlteId = req.body.id;
+    const categoria = req.body.cat;
+    const productoCollection = getCollection(categoria);
+    await productoCollection.doc(dlteId).delete();
+    res.redirect("/admin");
+},
 
-        editarProducto: async(req, res) => {
-            const id = req.body.id;
-            const {modelo, tamano, precio} = req.body;
-            const editProducto = {modelo, tamano, precio};
-            const categoria = req.body.cat;
-            const productoCollection = getCollection(categoria);
-            await productoCollection.doc(id).update(editProducto);
-            res.redirect("/admin");
-        }
-        
-    }
-
-    module.exports = controller;
+exports.editarProducto = async(req, res) => {
+    const id = req.body.id;
+    const {modelo, tamano, precio} = req.body;
+    const editProducto = {modelo, tamano, precio};
+    const categoria = req.body.cat;
+    const productoCollection = getCollection(categoria);
+    await productoCollection.doc(id).update(editProducto);
+    res.redirect("/admin");
+}

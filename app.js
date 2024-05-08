eda99c2f920e2e51efcce85b03f25aefaeffd103
gdimascio <guido.dimascio@gmail.com>
2024-05-08 12:42:32 -0300

    retoques css

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
+require("dotenv").config();
 
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 const hbs = require("hbs");
 const router = require ('./router/router')
